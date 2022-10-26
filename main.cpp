0f16f229d7ce658ec3e2ccd61e1732547ff843cf
soul-fb <R390serena4830>
2022-10-26 11:29:27 +0900

    therd Branchに修正

--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ int main (void)
 	std::cout << "Hello VisualStudio Git!" << std::endl;
 
 	std::cout << "Hello New Branch!" << std::endl;
+
+	std::cout << "therd Branch" << std::endl;
 	return 0 ;
 }
